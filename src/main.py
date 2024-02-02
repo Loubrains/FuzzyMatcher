e3d0036b2ca1a9b550502696f5e4ad88cb82ca72
Loubrains <louie.atk@gmail.com>
2024-02-02 13:14:31 +0000

    Updated logging

--- src/main.py
+++ src/main.py
@@ -6,26 +6,15 @@ from file_handler import FileHandler
 from logging_utils import setup_logging
 
 
-# def setup_logging():
-#     logging.basicConfig(
-#         level=logging.INFO,
-#         format="%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s",
-#         filename="app.log",
-#         filemode="w",
-#     )
-#     logging.getLogger("chardet").setLevel(logging.WARNING)
-
-#     logging.info("Logging initialized")
-
-
 def main():
-    logging.info("Application starting")
+    logger = logging.getLogger(__name__)
+    logger.info("Application starting")
     file_handler = FileHandler()
     data_model = DataModel(file_handler)
     user_interface = FuzzyUI()
     controller = Controller(user_interface, data_model)
     controller.run()
-    logging.info("Application terminated")
+    logger.info("Application terminated")
 
 
 if __name__ == "__main__":
